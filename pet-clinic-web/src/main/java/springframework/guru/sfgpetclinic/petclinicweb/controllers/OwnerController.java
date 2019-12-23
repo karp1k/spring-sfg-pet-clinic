4264e88d73f2788771e8c25be4487abbf16b1209
Tom <desktop-vm-ubuntu>
2019-12-23 15:49:39 +0300

    List of owners added to related controller Closes #16

--- pet-clinic-web/src/main/java/springframework/guru/sfgpetclinic/petclinicweb/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/springframework/guru/sfgpetclinic/petclinicweb/controllers/OwnerController.java
@@ -1,8 +1,10 @@
 package springframework.guru.sfgpetclinic.petclinicweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.guru.sfgpetclinic.petclinicdata.services.OwnerService;
 
 /**
  * @author kas
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = "/owners")
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @GetMapping(value = {"", "/", "/index", "/index.html"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
