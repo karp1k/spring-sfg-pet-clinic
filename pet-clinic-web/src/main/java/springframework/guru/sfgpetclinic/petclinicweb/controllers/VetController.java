6a540c10f09d11f33f96c574c7a2fa029dd7c192
Tom <desktop-vm-ubuntu>
2019-12-23 15:53:28 +0300

    List of vets added to related controller Closes #15

--- pet-clinic-web/src/main/java/springframework/guru/sfgpetclinic/petclinicweb/controllers/VetController.java
+++ pet-clinic-web/src/main/java/springframework/guru/sfgpetclinic/petclinicweb/controllers/VetController.java
@@ -1,8 +1,10 @@
 package springframework.guru.sfgpetclinic.petclinicweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springframework.guru.sfgpetclinic.petclinicdata.services.VetService;
 
 /**
  * @author kas
@@ -11,8 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping
-    public String getIndex() {
+    public String getIndex(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
