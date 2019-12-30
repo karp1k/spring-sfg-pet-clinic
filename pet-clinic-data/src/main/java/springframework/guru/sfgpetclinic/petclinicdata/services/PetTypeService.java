2f21274a8d11e7ba06ba0fd644c69f36c0ee650c
Tom <desktop-vm-ubuntu>
2019-12-30 15:46:59 +0300

    add PetType map Service impl Closes #29

---
+++ pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package springframework.guru.sfgpetclinic.petclinicdata.services;
+
+import springframework.guru.sfgpetclinic.petclinicdata.model.PetType;
+
+/**
+ * @author kas
+ */
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
