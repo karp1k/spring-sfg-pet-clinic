49ea11e87cb90c51fb6e82538d6eed44e2956f37
Tom <desktop-vm-ubuntu>
2019-12-30 14:10:25 +0300

    Add Visit Entity and add set of pets to owner Closes #26

--- pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Owner.java
+++ pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Owner.java
@@ -1,7 +1,19 @@
 package springframework.guru.sfgpetclinic.petclinicdata.model;
 
+import java.util.Set;
+
 /**
  * @author kas
  */
 public class Owner extends Person {
+
+    private Set<Pet> petSet;
+
+    public Set<Pet> getPetSet() {
+        return petSet;
+    }
+
+    public void setPetSet(Set<Pet> petSet) {
+        this.petSet = petSet;
+    }
 }
---
+++ pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Visit.java
@@ -0,0 +1,37 @@
+package springframework.guru.sfgpetclinic.petclinicdata.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author kas
+ */
+public class Visit extends BaseEntity {
+
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
