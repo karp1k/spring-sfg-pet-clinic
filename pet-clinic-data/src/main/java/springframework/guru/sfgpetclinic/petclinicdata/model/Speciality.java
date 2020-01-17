f1416ea75688b864966c6a26d9a906640eca53cd
Tom <desktop-vm-ubuntu>
2020-01-17 15:38:21 +0300

    Convert Speciality and Vet to Entity Closes #37

--- pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Speciality.java
+++ pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Speciality.java
@@ -1,10 +1,17 @@
 package springframework.guru.sfgpetclinic.petclinicdata.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author kas
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Vet.java
+++ pet-clinic-data/src/main/java/springframework/guru/sfgpetclinic/petclinicdata/model/Vet.java
@@ -1,13 +1,20 @@
 package springframework.guru.sfgpetclinic.petclinicdata.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 /**
  * @author kas
  */
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialitySet = new HashSet<>();
 
 
